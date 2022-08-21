3234a95da93e3a118a625c202def919a71eb536c
Marwan <marwansaad16@gmail.com>
2022-08-21 17:01:09 +0300

    Created a cloudinary cloud image upload

--- config/plugins.js
+++ config/plugins.js
@@ -1,10 +1,40 @@
-module.exports = ({ env }) => ({
-  email: {
-    config: {
-      provider: "sendgrid",
-      providerOptions: {
-        apiKey: env("SENDGRID_API_KEY"),
+module.exports = ({ env }) => {
+  if (env("NODE_ENV" === "production")) {
+    return {
+      upload: {
+        config: {
+          provider: "cloudinary",
+          providerOptions: {
+            cloud_name: env("CLOUDINARY_NAME"),
+            api_key: env("CLOUDINARY_KEY"),
+            api_secret: env("CLOUDINARY_SECRET"),
+          },
+          actionOptions: {
+            upload: {},
+            uploadStream: {},
+            delete: {},
+          },
+        },
+      },
+      email: {
+        config: {
+          provider: "sendgrid",
+          providerOptions: {
+            apiKey: env("SENDGRID_API_KEY"),
+          },
+        },
+      },
+    };
+  }
+
+  return {
+    email: {
+      config: {
+        provider: "sendgrid",
+        providerOptions: {
+          apiKey: env("SENDGRID_API_KEY"),
+        },
       },
     },
-  },
-});
+  };
+};
